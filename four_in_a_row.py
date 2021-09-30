cc89e9d1565ca8c9d559f0dd85214eae4c0b4e79
Maryam-ask <maryamaskari19@gmail.com>
2021-09-30 19:16:19 +0200

    Initial commit

---
+++ four_in_a_row.py
@@ -0,0 +1,152 @@
+'''
+Four in a row
+
+Author: Tony Lindgren
+
+Completed By: Maryam Askari and Mahtab BabaMohammadi
+'''
+from copy import deepcopy
+
+
+class FourInARow:
+    def __init__(self, player, chip):
+        new_board = []
+        for _ in range(7):
+            new_board.append([])
+        self.board = new_board
+        self.action = list(range(7))
+        if chip != 'r' and chip != 'w':
+            print('The provided value is not a valid chip (must be, r or w): ', chip)
+        if player == 'human' and chip == 'w':
+            self.ai_player = 'r'
+        else:
+            self.ai_player = 'w'
+        self.curr_move = chip
+
+    def to_move(self):
+        return self.curr_move
+
+    # actions
+    def actions(self):
+        """
+        A method to search for all possible action
+        :return: A list of all columns which has empty row
+        """
+        for c in range(len(self.board)):
+            if len(self.board[c]) < 6:
+                yield c
+
+    def result(self, action):
+        dc = deepcopy(self)
+        if self.to_move() == 'w':
+            dc.curr_move = 'r'
+            dc.board[action].append(self.to_move())
+        else:
+            dc.curr_move = 'w'
+            dc.board[action].append(self.to_move())
+        return dc
+
+    # eval
+    # TODO
+
+    def is_terminal(self):
+        # check vertical
+        for c in range(0, len(self.board)):  # Columns
+            count = 0
+            curr_chip = None
+            for r in range(0, len(self.board[c])):  # Rows
+                if curr_chip == self.board[c][r]:
+                    count = count + 1
+                else:
+                    curr_chip = self.board[c][r]
+                    count = 1
+                if count == 4:
+                    if self.ai_player == curr_chip:
+                        print('Found vertical win')
+                        return True, 100  # MAX ai wins positive utility
+                    else:
+                        print('Found vertical loss')
+                        return True, -100  # MIN player wins negative utility
+
+        # check horizontal
+        for r in range(6):
+            count = 0
+            curr_chip = None
+            for c in range(0, len(self.board)):  # Rows
+                if r < len(self.board[c]):
+                    if curr_chip == self.board[c][r]:
+                        count = count + 1
+                    else:
+                        curr_chip = self.board[c][r]
+                        count = 1
+                    if count == 4:
+                        if self.ai_player == curr_chip:
+                            print('Found horizontal win')
+                            return True, 100  # MAX ai wins positive utility
+                        else:
+                            print('Found horizontal loss')
+                            return True, -100
+
+        # check positive diagonal
+        for c in range(7 - 3):
+            for r in range(6 - 3):
+                if len(self.board[c]) > r and len(self.board[c + 1]) > r + 1 and len(self.board[c + 2]) > r + 2 and len(
+                        self.board[c + 3]) > r + 3:
+                    if self.ai_player == self.board[c][r] and self.ai_player == self.board[c + 1][
+                        r + 1] and self.ai_player == self.board[c + 2][r + 2] and self.ai_player == self.board[c + 3][
+                        r + 3]:
+                        print('Found positive diagonal win')
+                        return True, 100
+                    elif self.ai_player != self.board[c][r] and self.ai_player != self.board[c + 1][
+                        r + 1] and self.ai_player != self.board[c + 2][r + 2] and self.ai_player != self.board[c + 3][
+                        r + 3]:
+                        print('Found positive diagonal loss')
+                        return True, -100
+
+        # check negative diagonal
+        # TODO
+        for c in range(7 - 3):
+            for r in range(6 - 3, 6):
+                if len(self.board[c]) > r and len(self.board[c + 1]) > r - 1 and len(self.board[c + 2]) > r - 2 and len(
+                        self.board[c + 3]) > r - 3:
+                    if self.ai_player == self.board[c][r] and self.ai_player == self.board[c + 1][
+                        r - 1] and self.ai_player == self.board[c + 2][r - 2] and self.ai_player == self.board[c + 3][
+                        r - 3]:
+                        print('Found negative diagonal win')
+                        return True, 100
+                    elif self.ai_player != self.board[c][r] and self.ai_player != self.board[c + 1][
+                        r - 1] and self.ai_player != self.board[c + 2][r - 2] and self.ai_player != self.board[c + 3][
+                        r - 3]:
+                        print('Found negative diagonal loss')
+                        return True, -100
+
+        # check draw
+        # TODO
+        check_rows = 0
+        for c in range(len(self.board)):
+            if len(self.board[c]) == 6:
+                check_rows += 1
+        if check_rows == 6:
+            return True, 0
+
+        return False, 0
+
+
+        # pretty_print
+    def pretty_print(self):
+        """
+        A method to print out the board.
+        """
+        board_copy = deepcopy(self.board)
+
+        for c in range(len(board_copy)):
+            if len(board_copy[c]) != 6:
+                for _ in range(6 - len(board_copy[c])):
+                    board_copy[c].append("_")
+
+        for r in range(5, -1, -1):
+            for c in range(7):
+                print(board_copy[c][r], end=" ")
+            print()
+
+
---
+++ test.py
@@ -0,0 +1,35 @@
+# =============================================================================
+# Created By : Maryam Askari
+# Date: 9/29/2021
+# Time: 9:17 PM
+# =============================================================================
+"""The Module Has Been Build for..."""
+# =============================================================================
+# Imports
+# =============================================================================
+from four_in_a_row import FourInARow
+
+state0 = FourInARow('human', 'w')
+state0 = state0.result(0)
+state0 = state0.result(0)
+state0 = state0.result(0)
+state0 = state0.result(1)
+state0 = state0.result(1)
+state0 = state0.result(1)
+state0 = state0.result(2)
+state0 = state0.result(2)
+state0 = state0.result(2)
+state0 = state0.result(3)
+state0 = state0.result(3)
+state0 = state0.result(0)
+"""state0 = state0.result(0)
+state0 = state0.result(1)
+state0 = state0.result(2)
+state0 = state0.result(3)
+state0 = state0.result(4)"""
+
+print(state0.board)
+print(state0.actions())
+print(state0.pretty_print())
+# print(len(state0.board[2]))
+#print(state0.is_terminal())
